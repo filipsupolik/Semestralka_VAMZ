97b3fa63955c4518196836c95b8fb290474123a9
filipsupolik <filipsupolik25@gmail.com>
2025-04-03 09:46:07 +0200

    wokrking on pages with lists

--- app/src/main/java/com/example/vamz_semestralka_hero_journal_dnd/data/Lists.kt
+++ app/src/main/java/com/example/vamz_semestralka_hero_journal_dnd/data/Lists.kt
@@ -8,14 +8,68 @@ enum class RaceSize{
     SMALL, MEDIUM, LARGE
 }
 
-enum class RaceAttributes{
-    STR,
-    DEX,
-    CON,
-    INT,
-    WIS,
-    CHA
+enum class RaceAttributes(name: String){
+    STR("STR"),
+    DEX("DEX"),
+    CON("CON"),
+    INT("INT"),
+    WIS("WIS"),
+    CHA("CHA")
 }
+
+data class SubRace(
+    val name: String,
+    val extraStats: Map<String, Int> = emptyMap(),
+    val extraTraits: List<RaceTrait> = emptyList()
+)
+
+sealed class RaceTrait(
+    val name: String,
+    val desc: String
+) {
+    class SimpleTraits(name: String, desc: String): RaceTrait(name, desc)
+    class ChoiceTrats(
+        name: String,
+        desc: String,
+        val options: List<String>,
+        val selectedOption: String? = null
+    ): RaceTrait(name, desc)
+}
+
+sealed class HeroRaceDesc(
+    val name: String,
+    val age: String,
+    val size: RaceSize,
+    val speed: String,
+    val fixedLanguages: List<String>, // Always spoken (1 or 2)
+    val availableLanguages: List<String> = emptyList(), // Only if player picks 1
+    val baseStats: Map<String, Int>,
+    val baseTraits: List<RaceTrait>,
+    val subraces: List<SubRace> = emptyList()
+){
+    class AscendBorn: HeroRaceDesc(
+        name = "Ascend-Born",
+        age = "Ascended born mature at a similar rate humans do, and reaching adulthood in their late teens. They can live into the middle of their second century.",
+        size = RaceSize.MEDIUM,
+        speed = "30 ft. per turn",
+        fixedLanguages = listOf("Common"),
+        availableLanguages = listOf("Common", "Ionian", "Ixtali", "NorthSpeak", "Noxian", "Shuriman", "Valerian"),
+        baseStats = mapOf(RaceAttributes.CHA.name to 2),
+        baseTraits = listOf(
+            RaceTrait.SimpleTraits(name = "Darkvision", desc = "You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can't discern color in darkness, only shades of gray."),
+            RaceTrait.SimpleTraits(name = "Ascended Resistance", desc = "You have resistance to necrotic damage and radiant damage."),
+            RaceTrait.SimpleTraits(name = "Healing Hand", desc = "As an action, you can touch a creature and cause it to regain a number of hit points equal to your level. Once you use this trait, you can't use it again until you finish a long rest."),
+            RaceTrait.SimpleTraits(name = "Light Bearer", desc = "You know the light cantrip. Charisma is your spellcasting ability for it."),
+        ),
+        subraces = listOf(
+            SubRace(
+                name = "Protector",
+                extraStats = mapOf(RaceAttributes.WIS.name to 1)
+                )
+        )
+    )
+}
+
 
 sealed class Lists{
     data class Region(
@@ -24,17 +78,11 @@ sealed class Lists{
     ):Lists()
     data class HeroClasses(
         @DrawableRes val imageResourceId: Int,
-        val regionName: String
+        val className: String
     ):Lists()
     data class HeroRace(
         @DrawableRes val iconImageResourceId: Int,
-        @DrawableRes val mainImageResourceId: Int,
-        val raceName: String,
-        val raceBackStory: String,
-        val raceSpeed: Int,
-        val raceSize: RaceSize,
-        val raceAttributes: RaceAttributes,
-        val raceLanguages: List<String>
+        val raceName: String
     ):Lists()
 }
 
